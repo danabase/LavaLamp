0e43b8ba7786c2cb55398542dfe57a2ee846c0dc
Alyssa Kwan <alyssa.c.kwan@gmail.com>
2013-09-22 19:58:23 -0700

    XCode generated OpenGL

---
+++ BubbleTrouble/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  BubbleTrouble
+//
+//  Created by Alyssa Kwan on 9/22/13.
+//  Copyright (c) 2013 Dana Bienenfeld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface ViewController : GLKViewController
+
+@end
---
+++ BubbleTroubleTests/BubbleTroubleTests.m
@@ -0,0 +1,34 @@
+//
+//  BubbleTroubleTests.m
+//  BubbleTroubleTests
+//
+//  Created by Alyssa Kwan on 9/22/13.
+//  Copyright (c) 2013 Dana Bienenfeld. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BubbleTroubleTests : XCTestCase
+
+@end
+
+@implementation BubbleTroubleTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
