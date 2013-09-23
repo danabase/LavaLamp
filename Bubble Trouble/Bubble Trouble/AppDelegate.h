083210defe8147ba6d0559cdf7b7d79e9200d481
Alyssa Kwan <alyssa.c.kwan@gmail.com>
2013-09-22 19:46:37 -0700

    XCode generated OpenGL

---
+++ Bubble Trouble/Bubble Trouble/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Bubble Trouble
+//
+//  Created by Alyssa Kwan on 9/22/13.
+//  Copyright (c) 2013 Dana Bienenfeld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Bubble Trouble/Bubble Trouble/Shaders/Shader.fsh
@@ -0,0 +1,14 @@
+//
+//  Shader.fsh
+//  Bubble Trouble
+//
+//  Created by Alyssa Kwan on 9/22/13.
+//  Copyright (c) 2013 Dana Bienenfeld. All rights reserved.
+//
+
+varying lowp vec4 colorVarying;
+
+void main()
+{
+    gl_FragColor = colorVarying;
+}
